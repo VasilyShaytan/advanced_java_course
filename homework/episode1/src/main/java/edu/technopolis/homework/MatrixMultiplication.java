691d2c6269f80573e68c842daa8562caa1438011
Vasiliy Shaitan <vasiliy.shaitan@gmail.com>
2016-10-28 16:26:33 +0300

    add Multiply of Matrix

--- homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -1,5 +1,12 @@
 package edu.technopolis.homework;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Matrix multiplication home task.
  * <br/>
@@ -7,8 +14,53 @@ package edu.technopolis.homework;
  */
 public class MatrixMultiplication {
     public static void main(String... args) {
-        System.out.println("HelloWorld!!!");
-        System.out.println("HelloWorld!!!");
 
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int x = in.nextInt();
+        int y = in.nextInt();
+
+        if (n <= 0 | m <= 0 | x <= 0 | y <= 0) {
+            System.out.println("Некорректные аргументы!");
+            System.exit(0);
+        }
+        if (m != x) {
+            System.out.println("Матрицы невозможно перемножить!");
+            System.exit(0);
+        }
+        int lengthMatrixNM = n * m;
+        int lengthMatrixXY = x * y;
+        int[][] matrixNM = new int[n][m];
+        int[][] matrixXY = new int[x][y];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                matrixNM[i][j] = in.nextInt();
+            }
+        }
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++){
+                matrixXY[i][j] = in.nextInt();
+            }
+        }
+
+        int [][] matrixResult = new int [n][y];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < y; k++) {
+
+                    matrixResult[i][j] = matrixResult[i][j] + matrixNM[i][k] * matrixXY[k][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < y; j++){
+                System.out.print(matrixResult[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
